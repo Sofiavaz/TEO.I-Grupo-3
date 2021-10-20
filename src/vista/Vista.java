039b9818cd8452e64df555894fbe260751da3377
walter <wal.gs@hotmail.com>
2021-10-20 18:38:44 -0300

    Arreglo error en caso de archivo no existente

--- src/vista/Vista.java
+++ src/vista/Vista.java
@@ -11,8 +11,8 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class Vista extends JPanel{
-    private JTextArea urkPath;
-    private JTextField textoSalida;
+    private JTextArea textoSalida;
+    private JTextField textoUrl;
     private JButton botonAceptar;
     private JLabel labelUrl;
     private JLabel labelResult;
@@ -20,8 +20,8 @@ public class Vista extends JPanel{
 
     public Vista() {
         //construct components
-        urkPath = new JTextArea (5, 5);
-        textoSalida = new JTextField (5);
+        textoSalida = new JTextArea (5, 5);
+        textoUrl = new JTextField (5);
         botonAceptar = new JButton ("Aceptar");
         labelUrl = new JLabel ("Ingrese el URl de el texto a analizar");
         labelResult = new JLabel ("Resultados");
@@ -35,32 +35,33 @@ public class Vista extends JPanel{
             }}));
 
             //add components
-        add(urkPath);
         add(textoSalida);
+        add(textoUrl);
         add(botonAceptar);
         add (labelUrl);
         add (labelResult);
 
         //set component bounds (only needed by Absolute Positioning)
-        urkPath.setBounds (75, 140, 750, 330);
-        textoSalida.setBounds (80, 65, 635, 30);
+        textoSalida.setBounds (75, 140, 750, 330);
+        textoUrl.setBounds (80, 65, 635, 30);
         botonAceptar.setBounds (725, 65, 100, 30);
         labelUrl.setBounds (80, 35, 260, 25);
         labelResult.setBounds (75, 110, 750, 25);
     }
 
     public void agregarLinea(float linea){
-        urkPath.append(String.valueOf(linea) +'\n');
+        textoSalida.append(String.valueOf(linea) +'\n');
     }
     public void agregarLinea(String linea){
-        urkPath.append(linea+'\n');
+        textoSalida.append(linea+'\n');
     }
 
     public void start(){
         FileReader reader;
+        textoSalida.setText("");
         try {
             // Carga el archivo de texto desde url
-            reader = new FileReader(textoSalida.getText());
+            reader = new FileReader(textoUrl.getText());
 
             // Realiza el análisis lexicográfico.
             Lexico lexer = new Lexico(reader);
@@ -68,7 +69,7 @@ public class Vista extends JPanel{
             lexer.yylex();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
-            System.exit(-1);
+            agregarLinea("Archivo no encontrado");
         } catch (IOException e) {
             e.printStackTrace();
             System.exit(-1);
